49dfb2d08e9143320004d07188be199dfd6bf855
Xavier Lengelle <lengelle@gmail.com>
2016-05-10 10:35:57 +0200

    Ajout du modèle Produit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,12 @@
 class ProductsController < ApplicationController
 
-  PRODUCTS = [
-    {name: "kudoz", category: "pro"},
-    {name: "uSlide", category: "education"},
-    {name: "RuBum", category: "lifestyle"},
-    {name: "Roadstr", category: "lifestyle"}
-  ]
 
   def index
-    if params[:filter]
-      @products = PRODUCTS.select {|product| product[:category] == params[:filter]}
-    else
-      @products = PRODUCTS
-    end
+       @products = Product.all
   end
 
   def show
-    @product = PRODUCTS[params[:id].to_i]
+    @product = Product.find(params[:id])
   end
 
 end

5c739324dd4d2c0db9500254ec7602d7498c61e3
Xavier Lengelle <lengelle@gmail.com>
2016-05-10 17:28:28 +0200

    New data seed, ajout tagline, implémentations new et create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,10 +10,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,6 +29,12 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :url, :tagline)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+Product.destroy_all
+
+Product.create?(name: "Kudoz", url: "http://www.getkudoz.com", tagline:"Le tinder du recrutement")
+Product.create?(name: "Tinder", url: "http://www.tinder.com", tagline:"Le kudoz du dating")
+Product.create?(name: "Tatayoyo", url: "http://www.betatayoyo.com", tagline:"I'm no tatayoyo")
+Product.create?(name: "USlide", url: "http://www.uslide.com", tagline:"Youtube is not good for education")

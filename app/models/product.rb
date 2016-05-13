05cd4c6260bad1a968591fdbee9b48cfbd7fa03a
Xavier Lengelle <lengelle@gmail.com>
2016-05-13 16:38:22 +0200

    Ajout User dans product#new

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true
+  validates :user, presence: true
   belongs_to :category
   belongs_to :user
 end
